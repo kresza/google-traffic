5c79f32aa5d62bb082c9191f798b26a59ea0f071
Kresza1337 <krechaxdkrechaxd@gmail.com>
2024-03-10 01:09:25 +0100

    basic version

---
+++ traffic_monitor.py
@@ -0,0 +1,92 @@
+import googlemaps
+import json
+import csv
+from datetime import datetime
+from urllib.parse import urlparse, parse_qs
+import time
+import re
+from googlemaps import Client
+
+
+def get_coordinates_from_url(api_key, url):
+    """Get start and end coordinates from a Google Maps URL."""
+    gmaps: Client = googlemaps.Client(key=api_key)
+
+    parsed_url = urlparse(url)
+    query_params = parse_qs(parsed_url.query)
+    print("Parsed URL:", parsed_url)
+    print("Query Parameters:", query_params)
+
+    start_location = query_params.get('saddr', [''])[0]
+    end_location = query_params.get('daddr', [''])[0]
+
+    start_coordinates = get_coordinates(gmaps, start_location)
+    end_coordinates = get_coordinates(gmaps, end_location)
+
+    return start_coordinates, end_coordinates
+
+
+def get_coordinates(gmaps, location):
+    """Get coordinates (latitude, longitude) for a given location using Google Maps API."""
+    try:
+        geocode_result = gmaps.geocode(location)
+        if not geocode_result:
+            raise Exception(f"No results found for location: {location}")
+        return geocode_result[0]['geometry']['location']
+    except Exception as e:
+        print(f"Failed to get coordinates for location: {location}")
+        print(f"Error: {e}")
+        return None
+
+
+def calculate_route(api_key, route_url, log_response=False):
+    try:
+        gmaps = googlemaps.Client(key=api_key)
+
+        start_coordinates = (51.11245993415498, 17.055864466280198)  # Replace with actual start coordinates 51.11245993415498, 17.055864466280198
+        end_coordinates = (51.04044704259993, 16.96934186368963)
+
+        print("Calculating route...")
+
+        directions_result = gmaps.directions(
+            start_coordinates,
+            end_coordinates,
+            mode="driving",
+            departure_time=datetime.now(),
+        )
+        print("API Response:", directions_result)
+        if log_response:
+            with open('response.tmp.txt', 'w') as outfile:
+                outfile.write(json.dumps(directions_result, indent=2))
+
+        duration_in_traffic = directions_result[0]['legs'][0]['duration_in_traffic']['text']
+        minutes = int(re.search(r'(\d+) min', duration_in_traffic).group(1))
+
+        day_of_week = datetime.today().weekday()
+        date = datetime.today().strftime('%Y.%m.%d')
+        time_of_day = datetime.now().strftime("%H:%M")
+
+        print(f"Route calculated. Waiting for 2 seconds...")
+        time.sleep(2)
+
+        # Save the route data to a CSV file
+        with open('route_times.csv', 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([date, time_of_day, start_coordinates, end_coordinates, minutes])
+
+        print(f"Duration in traffic: {duration_in_traffic}")
+
+    except Exception as e:
+        print(f"Error calculating route: {e}")
+
+
+if __name__ == "__main__":
+    api_key = "AIzaSyDjArNix8I0akx1uouwhcAO3743fLT6ncE"
+
+    # Replace 'YOUR_GOOGLE_MAPS_URL' with your actual Google Maps URL
+    google_maps_url = 'https://maps.app.goo.gl/V75gsHxx1otxYxoT9'
+
+    # Run the function
+    calculate_route(api_key, google_maps_url)
+
+
